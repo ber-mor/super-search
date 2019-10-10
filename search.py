2a13b768508fb33b17adea203a056430c7c7acc8
Bernardo Morcon <bernardo.morcon@gmail.com>
2019-10-10 01:06:51 -0600

    Deleted loop through dictionary and simplified search string manipulation

--- search.py
+++ search.py
@@ -3,19 +3,7 @@ import os
 
 os.system('mode con: cols=50 lines=5')
 
-while(True):
-
-	# Removes unnecesary spaces 
-	raw_search = ' '.join(input('Search here: ').split())
-
-	# Asks for input again if the first one was empty or just spaces
-	while raw_search == '':
-		raw_search = ' '.join(input('Nice try. Try again: ').split())	
-	
-	# Removes first 3 characters from the original string: 2 from id and 1 space
-	search = raw_search[3:]
-
-	search_engines = {
+search_engines = {
 		'gg': 'https://www.google.com/search?q=',
 		'yt': 'https://www.youtube.com/results?search_query=', 
 		'ml': 'https://listado.mercadolibre.com.mx/',
@@ -23,14 +11,22 @@ while(True):
 		'bl': 'https://borderlands.fandom.com/wiki/Special:Search?query='
 	}
 
-	for se in search_engines:
-		# Compares the current key with the 2 first characters of the search string
-		if se == raw_search[:2]:
-			# Opens new tab with composed link	
-			webbrowser.open_new(search_engines[se]+search)	
-			break
+while(True):
+	# Removes unnecesary spaces 
+	raw_search = ' '.join(input('Search here: ').split())
 
-		# Compares the current key with the last one of the dict
-		elif se == list(search_engines.items())[-1][0]:
-			#Opens default google search if search engine wasn't specified
-			webbrowser.open_new(search_engines['gg']+raw_search)
+	# Asks for input again if the first one was empty or it was just spaces
+	while raw_search == '':
+		raw_search = ' '.join(input('Nice try. Try again: ').split())	
+	
+	# Removes key from original search string, including the space after it  
+	search = ' '.join(raw_search.split()[1:])
+
+	# Gets key from original search string
+	search_key = raw_search.split()[0]
+
+	if search_key in search_engines.keys():
+		webbrowser.open_new(search_engines[search_key]+search)
+	else:
+		# Default case: opens google search if search engine was not specified
+		webbrowser.open_new(search_engines['gg']+raw_search)
